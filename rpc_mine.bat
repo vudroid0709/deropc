7da7aee6fc81ec5ba1a5b2f81c3724628071bbd8
vudroid0709 <128033809+vudroid0709@users.noreply.github.com>
2023-03-30 21:16:07 +0700

    Add files via upload

---
+++ rpc_mine.bat
@@ -0,0 +1,7 @@
+:loop
+
+astrominer.exe -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcmdj2765e9vt9snp8srt -m 8 -r community-pools.mysrv.cloud:10300 -p rpc
+
+timeout /t 5
+
+goto loop
